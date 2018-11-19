7a80f4137494ed48fd6db4fec1deb687e29ff0d5
ChengTsang <34964022+ChengTsang@users.noreply.github.com>
2018-11-19 21:56:21 +0800

    Update regAndPre.m

--- regAndPre.m
+++ regAndPre.m
@@ -1,31 +1,26 @@
 function fStar=regAndPre(x0,y0,yadta,A,Hyp)
-%      tic
-%          persistent ydata1;
-%          persistent A1;
-%          persistent Hyp1;
-%      t=toc
-    %本程序是在regAndPre基础上的改进;
-    %无参数时执行第一部分，为训练提供数据，需要解很多次pde;
-    %有一个参数时执行第二部分，进行矩阵与向量之间的转化;
-    %两个参数执行第三部分,得到观察向量;
+    % When there is no parameter, the first part is to provide data for training, and it needs to solve many times PDE.
+
+    % When a parameter has second parameters, it performs the transformation between the matrix and the vector.
+
+    % Two parameters are executed in third parts, and the observation vectors are obtained.
     
-    %第一部分
+    % first part
     if nargin==0
-        %gauss回归做的准备，先解pde
         i=1;
         for x1= 0.1:0.1:1
             for x2= 0.1:0.1:1
-                udata_2{i}=fun5_1(x1,x2);%开元胞数组来存数据,有100个元胞放这些矩阵，每个矩阵是11*11的
+                %Using cell array to store data, there are 100 cells to put these matrices, each matrix is 11*11.
+                udata_2{i}=fun5_1(x1,x2);
                 i=i+1;
             end
         end
-       % udata{1003}
-       save('udata_2.mat','udata_2');
+        save('udata_2.mat','udata_2');
     end
     
-    %第二部分
+    % second part
     if nargin==1
-        %针对每个分量的回归
+        % Regression for each component
         %whos -file udata.mat;
         load('udata_2.mat');
         udata_2{1};
@@ -34,20 +29,22 @@ function fStar=regAndPre(x0,y0,yadta,A,Hyp)
                 if mod(i-1,11)==0
                     ydata{i}(j)=udata_2{j}((floor((i-1)/11)+1),1);
                 else
-                    ydata{i}(j)=udata_2{j}((floor((i-1)/11)+1),mod(i-1,11)+1);%11*11维矩阵的每个分量组成的100维向量
+                    %The 100 dimensional vector of each component of the 11*11 dimensional matrix.
+                    ydata{i}(j)=udata_2{j}((floor((i-1)/11)+1),mod(i-1,11)+1);
             
                 end
             end
         end
         save('ydata.mat','ydata')
-      %得到超参数  
+      % get hyperparameter
          for i=1:1:121
-             Hyp{i}=exp(getHyp(ydata{i}')) ;%Udata是训练用的每个分量上数据集，每个Hyp是一个三维向量
+             %Udata is the data set for each component of training, and each Hyp is a three-dimensional vector.
+             Hyp{i}=exp(getHyp(ydata{i}')) ;
          end
          
          
         save('Hyp.mat','Hyp');
-      %得到中间矩阵
+      % Get intermediate matrix
       [x1,x2] = meshgrid(linspace(0.1,1,10)); x = [x1(:),x2(:)];
         for k=1:1:121
             for i=1:1:100
@@ -57,7 +54,7 @@ function fStar=regAndPre(x0,y0,yadta,A,Hyp)
             end
         end
          save('K.mat','K');
-       %得到需要保存的矩阵A\y
+       % Get the matrix A\y that needs to be saved.
        for i=1:1:121
            A{i}=(K{i}+(Hyp{i}(3))^2*eye(100))\(ydata{i}');
        end
@@ -65,8 +62,8 @@ function fStar=regAndPre(x0,y0,yadta,A,Hyp)
      K{1};
     end
         
-    %预测
-    %第三部分
+    % Predict
+    % the third part 
     if nargin==5
         xStar=[x0,y0];
         xStar;
